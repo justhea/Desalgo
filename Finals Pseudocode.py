2aad37c75192c6bf812ef12440c6ee9153939f7b
asdasd <jufranhundo@gmail.com>
2022-11-14 20:39:58 +0800

    Added sorting
    Added sorting for basis score and sorted score

--- Finals Pseudocode.py
+++ Finals Pseudocode.py
@@ -2,54 +2,44 @@ from psychologyQuestionnaire import Psychology
 from nursingQuestionnaire import Nursing
 from pharmacologyQuestionnaire import Pharmacology
 from medTechQuestionnaire import MedicalTechnology
+import time
+basisScore = []
+sortedScore = []
+basisScore.append(Psychology())
+basisScore.append(Nursing())
+basisScore.append(MedicalTechnology())
+basisScore.append(Pharmacology())
+
+for i in sortedScore:
+    sortedScore.append(i)
+print(basisScore)
+
+courses = ["Pyschology", "Nursing", "Medical Technology", "Pharmacy"]
 
 def insertionSort(arr):
- 
-    # Traverse through 1 to len(arr)
     for i in range(1, len(arr)):
-    
+ 
         key = arr[i]
-    
+ 
         # Move elements of arr[0..i-1], that are
         # greater than key, to one position ahead
         # of their current position
         j = i-1
-        while j >= 0 and key < arr[j] :
+        while j >= 0 and key > arr[j] :
                 arr[j + 1] = arr[j]
                 j -= 1
         arr[j + 1] = key
-def search(arr, N, x):
  
-    for i in range(0, N):
-        if (arr[i] == x):
+insertionSort(sortedScore)
+def linearSearch(basisScore, n, y):
+    for i in range(0, n):
+        if (basisScore[i] == y):
             return i
     return -1
- 
-# Driver Code
-if __name__ == "__main__":
-    arr = [2, 3, 4, 10, 40]
-    x = 10
-    N = len(arr)
-    
-    # Function call
-    result = search(arr, N, x)
+for y in sortedScore:
+    n = len(sortedScore)
+    result = linearSearch(basisScore, n, y)
     if(result == -1):
-        print("Element is not present in array")
+        print("Element not found")
     else:
-        print("Element is present at index", result)
-    
-# Driver code to test above
-arr = [12, 11, 13, 5, 6]
-insertionSort(arr)
-for i in range(len(arr)):
-    print ("% d" % arr[i])
-
-#Instructions
-print("Instructions:")
-print("Multiple Choice. Choose the most suitable choice for the given question/situation.")
-print("Your score will be tallied and sorted to procure your suitable course based on your scores. \n")
-
-Psychology()
-Nursing()
-Pharmacology()
-MedicalTechnology()
+	    print(courses[result])
